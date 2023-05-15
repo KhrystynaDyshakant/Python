fbaa6e17a8130b8bc1a02a0b7ff6732277cedcac
KhrystynaDyshkant <khrystynadiskant@gmail.com>
2023-05-15 19:36:19 +0300

    added with fixes

--- lab1.py
+++ lab1.py
@@ -1,44 +1,68 @@
 class Hotel:
-    instance = None
+    __instance = None
 
-    def __init__(self, name="Zirka", totalRooms=4, availableRooms=5, rating=5):
-        self.name = name
-        self.totalRooms = totalRooms
-        self.availableRooms = availableRooms
-        self.rating = rating
+    def __init__(self, name="Zirka", total_rooms=4, available_rooms=5, rating=5):
+        self._name = name
+        self._total_rooms = total_rooms
+        self._available_rooms = available_rooms
+        self._rating = rating
 
     @staticmethod
-    def getInstance():
-        if Hotel.instance is None:
-            Hotel.instance = Hotel()
-        return Hotel.instance
+    def get_instance():
+        if not Hotel.__instance:
+            Hotel.__instance = Hotel()
+        return Hotel.__instance
 
-    def bookRoom(self):
-        if self.availableRooms > 0:
-            self.availableRooms -= 1
+    def book_room(self):
+        if self._available_rooms > 0:
+            self._available_rooms -= 1
 
-    def releaseRoom(self):
-        self.availableRooms += 1
+    def release_room(self):
+        self._available_rooms += 1
 
-    def getAvailableRooms(self):
-        return self.availableRooms
-
-    def getBookedRoomsCount(self):
-        return self.totalRooms - self.availableRooms
+    def get_booked_rooms_count(self):
+        return self._total_rooms - self._available_rooms
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = new_name
+        
+    @property
+    def total_rooms(self):
+        return self._total_rooms
+    
+    @total_rooms.setter
+    def total_rooms(self, total_rooms):
+        self._total_rooms = new_total_rooms
+        
+    @property
+    def available_rooms(self):
+        return self._available_rooms
+    
+    @available_rooms.setter
+    def available_rooms(self, available_rooms):
+        self._available_rooms = new_available_rooms
+        
+    @property
+    def rating(self):
+        return self._rating
+    
+    @rating.setter
+    def rating(self, rating):
+        self._rating = new_rating
 
     def __str__(self):
-        return f"Hotel: {self.name}, Total Rooms: {self.totalRooms}, Available Rooms: {self.availableRooms}, Rating: {self.rating}"
-
+        return f"Hotel: {self._name}, Total Rooms: {self._total_rooms}, Available Rooms: {self._available_rooms}, Rating: {self._rating}"
 
 hotel1 = Hotel()
 hotel2 = Hotel("Grand", 100, 80, 4)
-hotel3 = Hotel.getInstance()
-hotel4 = Hotel.getInstance()
+hotel3 = Hotel.get_instance()
+hotel4 = Hotel.get_instance()
 hotels = [hotel1, hotel2, hotel3, hotel4]
 
 for hotel in hotels:
     print(hotel)
-
-
-
-
